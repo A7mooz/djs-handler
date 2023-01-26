80b41d78e4fc4794ee246ac80d5aac939e6a22cd
A7mooz <teaming04@gmail.com>
2023-01-26 20:53:53 +0300

    Fixed scandir bug

--- src/helpers/templates.ts
+++ src/helpers/templates.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
+import { join } from 'path';
 
-export const templatesDir = 'templates';
+export const templatesDir = join(process.cwd(), 'templates');
 
 export const templates = fs.readdirSync(templatesDir);
